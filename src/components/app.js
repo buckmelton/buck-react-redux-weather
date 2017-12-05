1f02a2dbcf9b493484d2a704fa517f0f6b54f4b2
Buck Melton <bmelton@mdsol.com>
2017-12-05 13:28:59 -0800

    Add search bar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar.js';
 
 export default class App extends Component {
   render() {
     return (
-      <div>"Buck's Weather App"</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
