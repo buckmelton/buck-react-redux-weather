d3b25da82b1d819552777ba8cc4d12dda1bcd696
buckmelton <buck9063@gmail.com>
2017-12-06 13:09:45 -0800

    Add Action Creator for fetching weatber data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,12 @@
 # This is a public key
 const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER;
+    payload: request;
+  }
+}
