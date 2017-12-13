1c4c5fc6eca14b34adbac12bfefe3a3c51cd9ce7
buckmelton <buck9063@gmail.com>
2017-12-13 15:58:17 -0800

    Start rendering city weather data

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,13 +4,11 @@ import axios from 'axios';
 const API_KEY = '41ef477551042217f4042b3932a76d9e';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
 
-const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-
-  console.log('Request: ', request);
 
   return {
     type: FETCH_WEATHER,
